6f0dba2f84951c659f9a66476d6bf98e64b648b9
Michael Knightly <99145919+mknightly2000@users.noreply.github.com>
2025-04-15 22:26:11 -0700

    feat: implement func to draw xs and os

--- main.rb
+++ main.rb
@@ -1,11 +1,37 @@
-require 'ruby2d'
+require "ruby2d"
 
 set title: "Tic Tac Toe", width: 300, height: 300
 
+# constants
+$background_color = ""#000000""
+$s = 100  # square_side_length
+$padding_around_sign = 10
+$sign_width = 3
+
+set background: $background_color
+
 # grid lines
-Line.new(x1: 100, y1: 0, x2: 100, y2: 300, width: 2, color: 'black')
-Line.new(x1: 200, y1: 0, x2: 200, y2: 300, width: 2, color: 'black')
-Line.new(x1: 0, y1: 100, x2: 300, y2: 100, width: 2, color: 'black')
-Line.new(x1: 0, y1: 200, x2: 300, y2: 200, width: 2, color: 'black')
+Line.new(x1: $s, y1: 0, x2: $s, y2: 3 * $s, width: $sign_width, color: "black")
+Line.new(x1: 2 * $s, y1: 0, x2: 2 * $s, y2: 3 * $s, width: $sign_width, color: "black")
+Line.new(x1: 0, y1: $s, x2: 3 * $s, y2: $s, width: $sign_width, color: "black")
+Line.new(x1: 0, y1: 2 * $s, x2: 3 * $s, y2: 2 * $s, width: $sign_width, color: "black")
+
+def draw_x(row, col)
+  x = col * $s + $padding_around_sign
+  y = row * $s + $padding_around_sign
+
+  sign_size = $s - 2 * $padding_around_sign
+  Line.new(x1: x, y1: y, x2: x + sign_size, y2: y + sign_size, width: 10, color: "red")
+  Line.new(x1: x + sign_size, y1: y, x2: x, y2: y + sign_size, width: 10, color: "red")
+end
+
+def draw_o(row, col)
+  x = col * $s + ($s / 2)
+  y = row * $s + ($s / 2)
+  Circle.new(x: x, y: y, radius: $s / 2 - $padding_around_sign, sectors: 64, color: "blue")
+  Circle.new(x: x, y: y, radius: $s / 2 - $padding_around_sign - $sign_width, sectors: 64, color: $background_color)
+end
+
+
 
 show
